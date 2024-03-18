a444959a1e0f5a97b956f43468a88b18afcb683c
unknown <jithinbinoy2000@gmail.com>
2024-03-18 12:37:37 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+})
